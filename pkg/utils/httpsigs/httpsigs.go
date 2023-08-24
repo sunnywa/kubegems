3f6e38416e1f2bf3c9ae6ad0b1fef91c2cb907cf
王琦 <1099046071@qq.com>
2023-08-24 20:12:07 +0800

    Update httpsigs.go

--- pkg/utils/httpsigs/httpsigs.go
+++ pkg/utils/httpsigs/httpsigs.go
@@ -87,7 +87,7 @@ func (s *Signer) Validate(req *http.Request) error {
 	n := time.Now()
 	after := n.Add(time.Second * time.Duration(s.Duration)).Unix()
 	before := n.Add(time.Second * (-1 * time.Duration(s.Duration))).Unix()
-	if true || timestamp > after || timestamp < before {
+	if false && (timestamp > after || timestamp < before) {
 		return fmt.Errorf("httpsigs time out, origin: %s, now: %v", timeStr, n)
 	}
 	toSignStr := path + timeStr + s.Token
